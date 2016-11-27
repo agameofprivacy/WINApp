4d431d81b15172c76f753f7c06615abecd343e75
Eddie Chen <agameofprivacy@gmail.com>
2016-11-26 21:39:02 -0700

    activity indicator bg color fix, listview height fix

--- components/ProgramsList.js
+++ components/ProgramsList.js
@@ -1,11 +1,14 @@
 import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
+import Dimensions from 'Dimensions';
 
 const ProgramListItem = require('../components/ProgramListItem.js');
 
-const { View, ToolbarAndroid, Text, StyleSheet, ListView, Alert} = ReactNative;
+const { View, ToolbarAndroid, Text, StyleSheet, ListView, Alert, ActivityIndicator} = ReactNative;
 class ProgramsList extends React.Component{
+
+
 
   constructor(props){
     super(props);
@@ -56,8 +59,10 @@ class ProgramsList extends React.Component{
         programs[programs.length - 1][lgbtqSpecialtyKey] = child.val()[lgbtqSpecialtyKey];
         programs[programs.length - 1][veteransSpecialtyKey] = child.val()[veteransSpecialtyKey];
       });
+      // remove activity indicator
       this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(programs)
+        dataSource: this.state.dataSource.cloneWithRows(programs),
+        animating:false
       });
     });
   }
@@ -77,13 +82,22 @@ class ProgramsList extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <ToolbarAndroid style={styles.toolbar}
-                        navIcon={{uri:"ic_arrow_back_white_24dp"}}
-                        title={this.props.passProps.title}
-                        onIconClicked={this.props.navigator.pop}
-                        actions={[{title: 'Filter', icon: {uri:"ic_filter_list_white_24dp"}, show: 'always', showWithText:true}]}
-                        titleColor={'#FFFFFF'}/>
-        <ListView dataSource={this.state.dataSource} renderRow={this._renderProgram.bind(this)} enableEmptySections={true} style={styles.listView} />
+        <View style={styles.fullScreen}>
+          <ToolbarAndroid style={styles.toolbar}
+                  navIcon={{uri:"ic_arrow_back_white_24dp"}}
+                  title={this.props.passProps.title}
+                  onIconClicked={this.props.navigator.pop}
+                  actions={[{title: 'Filter', icon: {uri:"ic_filter_list_white_24dp"}, show: 'always', showWithText:true}]}
+                  titleColor={'#FFFFFF'}/>
+          <ListView dataSource={this.state.dataSource} renderRow={this._renderProgram.bind(this)} enableEmptySections={true} style={styles.listView} />
+        </View>
+        <View style={styles.floatView}>
+          <ActivityIndicator
+            animating={this.state.animating}
+            style={[styles.activityIndicator]}
+            size="large"
+          />
+        </View>
       </View>
     );
   }
@@ -97,15 +111,27 @@ class ProgramsList extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
   },
   toolbar: {
     backgroundColor:'#009688',
     height:56,
   },
   listView: {
-    flex:1,
+    // flex:1,
     backgroundColor:'#ffffff'
+  },
+  activityIndicator:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  floatView:{
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    top: Dimensions.get('window').height / 2 - 50,
+    left: Dimensions.get('window').width / 2 - 50,
   },
 });
 
